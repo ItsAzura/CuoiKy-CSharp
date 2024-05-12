086646e80454fc2df0b61ed1a3e645a32563470d
ItsAzura <azurawithluz@gmail.com>
2024-05-12 21:38:22 +0700

    Add Department

---
+++ CK-CSharp/Controllers/DepartmentController.cs
@@ -0,0 +1,45 @@
+﻿using CK_CSharp.Data;
+using CK_CSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CK_CSharp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly EmployeeDbContext dbContext;
+
+        public DepartmentController(EmployeeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new Department();
+            var companies = dbContext.Companies.ToList();
+            //model.Companies = companies;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Department departments)
+        {
+            var department = new Department
+            {
+                Name = departments.Name,
+                Description = departments.Description,
+                CompanyId = departments.CompanyId
+            };
+
+            await dbContext.Departments.AddAsync(department);
+
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+
+    }
+}
--- CK-CSharp/obj/Debug/net8.0/CK-CSharp.AssemblyInfo.cs
+++ CK-CSharp/obj/Debug/net8.0/CK-CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567b875bfa9a843616f3d9bc234464fccfa8b78c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7302dec399f27bd9ceede0fd7d219509aa0934e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
