3c270feb690ae87d1d35a90d255f626737c3492a
ItsAzura <azurawithluz@gmail.com>
2024-05-20 08:39:43 +0700

    soft by name in Company

--- CK-CSharp/obj/Debug/net8.0/CK-CSharp.AssemblyInfo.cs
+++ CK-CSharp/obj/Debug/net8.0/CK-CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ba7e69edb6d48d22ca69739caa77ad611dc479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f6b7c2ce7559bddb212bb13a3a47e508eda402")]
 [assembly: System.Reflection.AssemblyProductAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CK-CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
